cd0761f247196f48512b37ba8a7f2efbbf598186
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-02-20 20:57:26 +0400

    Create elnur.py

---
+++ elnur.py
@@ -0,0 +1,13 @@
+from telethon import TelegramClient 
+  
+ # Config məlumatları 
+  
+ # Telegram Client (Telethon) 
+API_ID = 29918051
+API_HASH = "793e62b1b2aefe53f84976d38215959e" 
+bot_token = "5746131579:AAEDzPqfVsHst13-AlVCxs3Z2rZqOly8fPw" 
+SUDO_USERS = 5317589296
+log_qrup = -1001875414285
+
+ # Elnur
+elnur = TelegramClient('elnur', API_ID, API_HASH).start(bot_token=bot_token)
