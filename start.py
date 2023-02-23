57ed1a3bddf16bbccabc7522369a6b1febe447ea
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-02-24 00:13:15 +0400

    update start.py

--- start.py
+++ start.py
@@ -119,7 +119,12 @@ async def handle_delete(event):
         message = await event.get_reply_message()
         await elnur.delete_messages(chat, message)
 
-
+@elnur.on(events.NewMessage(pattern='/purge'))
+async def delete_all_messages(event):
+    msg = await event.reply('Bütün mesajlar silinir...')
+    messages = await event.elnur.get_messages(event.chat_id)
+    await event.elnur.delete_messages(event.chat_id, messages)
+    await msg.edit('Bütün mesajlar silindi.')
 
 @elnur.on(events.NewMessage(pattern='/bots'))
 async def show_bots(event):
