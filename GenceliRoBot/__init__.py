6b353e820a0f69d237e9fbf04bb08349c45167c1
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-03-05 16:49:05 +0400

    Update __init__.py

--- GenceliRoBot/__init__.py
+++ GenceliRoBot/__init__.py
@@ -1,1 +1,16 @@
+import logging
+from pyrogram import Client
+from GenceliRoBot.elnur import elnur
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+LOGGER = logging.getLogger(__name__)
+
+AylinRobot = Client(
+    'AylinRobot',
+    bot_token = Config.bot_token,
+    api_id = Config.API_ID,
+    api_hash = Config.API_HASH
+)
