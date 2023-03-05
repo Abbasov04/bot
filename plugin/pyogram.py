0bcae1abffc624fbbf3ced50ae744c3bac5966cb
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-03-05 13:33:01 +0400

    Update pyogram.py

--- plugin/pyogram.py
+++ plugin/pyogram.py
@@ -1,1 +1,14 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message,
 
+from asyncio import sleep
+
+@Client.on_message(filters.command(["restart"]))
+async def restart(_, message: Message):
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("<b>Heroku Veriləri Yenilənir Zəhmət Olmasa Gözləyin</b>")
+        if not message.reply_to_message:
+            await sleep(10)
+            await wtf.edit("<b>✅Heroku Uğurla Yeniləndi</b>")
