ba54fef08034756180f06b53836ade0d095206f7
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-03-05 13:35:58 +0400

    Update pyogram.py

--- plugin/pyogram.py
+++ plugin/pyogram.py
@@ -3,12 +3,6 @@ from pyrogram.types import Message,
 
 from asyncio import sleep
 
-@Client.on_message(filters.command(["restart"]))
-async def restart(_, message: Message):
-    if message.from_user.id not in SUDO_USERS:
-        return
-    else:
-        wtf = await message.reply("<b>Heroku Veriləri Yenilənir Zəhmət Olmasa Gözləyin</b>")
-        if not message.reply_to_message:
-            await sleep(10)
-            await wtf.edit("<b>✅Heroku Uğurla Yeniləndi</b>")
+@Client.on_message(command(["sudo"]))
+async def sudo(_, message: Message):
+      await message.reply_text("⭐️ Sahiblər:\n\n1➤ @ElnurGenCeLi\n2➤ @rapunzzel_z")
