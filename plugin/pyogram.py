268e9df83cf5d1b557c73ab66c6b23af3dd7ce6f
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-03-05 13:37:08 +0400

    Delete pyogram.py

--- plugin/pyogram.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message,
-
-from asyncio import sleep
-
-@Client.on_message(command(["sudo"]))
-async def sudo(_, message: Message):
-      await message.reply_text("⭐️ Sahiblər:\n\n1➤ @ElnurGenCeLi\n2➤ @rapunzzel_z")
