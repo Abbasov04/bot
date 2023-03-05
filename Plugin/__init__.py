91712b1aeab235c3d193becf24c96ebbeb1f7006
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-03-05 16:24:42 +0400

    Delete Plugin directory

--- Plugin/__init__.py
+++
@@ -1,7 +0,0 @@
-from os.path import dirname, basename, isfile, join
-import glob
-
-modules = glob.glob(join(dirname(file), "*.py"))
-all = [
-    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("init.py")
-]
