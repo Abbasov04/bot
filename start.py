f2ad39656ee2ec8612dde5d01ad9371378042318
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-03-28 00:10:13 +0400

    Update start.py

--- start.py
+++ start.py
@@ -21,7 +21,7 @@ LOGGER = logging.getLogger(__name__)
 # config 
 API_ID = 29918051
 API_HASH = "793e62b1b2aefe53f84976d38215959e"
-bot_token = "5746131579:AAEDzPqfVsHst13-AlVCxs3Z2rZqOly8fPw"
+bot_token = "5746131579:AAEL2ySw1sVRwsFdqekn9L4QO6mix6do9zE"
 
 
 anlik_calisan = []
