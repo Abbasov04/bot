45603d78eebb855eb75fdb9a8c96dd858ecc823f
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-03-28 14:32:35 +0400

    update elnur.py

--- elnur.py
+++ elnur.py
@@ -5,7 +5,7 @@ from telethon import TelegramClient
  # Telegram Client (Telethon) 
 API_ID = 29918051
 API_HASH = "793e62b1b2aefe53f84976d38215959e" 
-bot_token = "5746131579:AAEDzPqfVsHst13-AlVCxs3Z2rZqOly8fPw" 
+bot_token = "5746131579:AAEL2ySw1sVRwsFdqekn9L4QO6mix6do9zE" 
 SUDO_USERS = 5317589296
 log_qrup = -1001875414285
 
