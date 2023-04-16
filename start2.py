9789015276b91b303f6ec1b5afc63c91da948233
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-04-16 13:59:09 +0400

    Delete start2.py

--- start2.py
+++
@@ -1,5 +0,0 @@
-from telethon import events, TelegramClient
-
-@elnur.on(events.NewMessage(pattern='/start2'))
-async def start_handler(event):
-    await event.respond('Salam! Botumuzla əlaqə yaratdığınız üçün təşəkkür edirik.')
