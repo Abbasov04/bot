a6e77c505781868365c9b3ea11cf32e1267740fd
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-04-16 14:25:05 +0400

    Update start2.py

--- Genceli/start2.py
+++ Genceli/start2.py
@@ -1,10 +1,10 @@
 from telethon import TelegramClient, events
 
-API_ID = 29918051
-API_HASH = "793e62b1b2aefe53f84976d38215959e"
+api_id = 29918051
+api_hash = "793e62b1b2aefe53f84976d38215959e"
 bot_token = "5746131579:AAEL2ySw1sVRwsFdqekn9L4QO6mix6do9zE"
 
-elnur = TelegramClient('elnur', API_ID, API_HASH).start(bot_token=bot_token)
+elnur = TelegramClient('elnur', api_id, api_hash).start(bot_token=bot_token)
 
 # Bot komutları için event handler
 @elnur.on(events.NewMessage(pattern='/start2'))
