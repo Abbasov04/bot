6502ad0fbc32f7554d315eef22e6214d8f72d17d
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-04-16 14:07:18 +0400

    Create __main__.py

---
+++ Genceli/__main__.py
@@ -0,0 +1,14 @@
+from telethon import TelegramClient, events
+
+api_id = 29918051  # API ID-nizi burada daxil edin
+api_hash = '793e62b1b2aefe53f84976d38215959e'  # API HASH-nizi burada daxil edin
+bot_token = '5746131579:AAEL2ySw1sVRwsFdqekn9L4QO6mix6do9zE'  # Botunuzun tokeni burada daxil edin
+
+elnur = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
+
+@elnur.on(events.NewMessage(pattern='/hello'))
+async def handler(event):
+    await event.respond('Hello, World!')
+
+if __name__ == '__main__':
+    bot.run_until_disconnected()
