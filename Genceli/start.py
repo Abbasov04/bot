176e27b3664c3a6646a4204f3c9bc10783d2ab98
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-04-17 22:47:45 +0400

    update start.py

--- Genceli/start.py
+++ Genceli/start.py
@@ -102,52 +102,6 @@ async def start(event):
                     link_preview=False
                    )
 
-@elnur.on(events.NewMessage(pattern="^.help ?(.*)"))
-async def help(event):
-        await event.reply(f"[{BOT_NAME}](https://t.me/{BOT_USERNAME}) Botun Əmirləri:",
-        buttons=(
-                      [
-                       Button.inline("Sudo", data="sudo")
-                      ],[
-                       Button.inline("Hami", data="hami")
-                      ],
-                    ),
-                    link_preview=False)
-                    
-
-@elnur.on(events.callbackquery.CallbackQuery(data="helpdata"))
-async def handler(event):
-    await event.edit(f"[{BOT_NAME}](https://t.me/{BOT_USERNAME}) Botun Əmirləri:",
-        buttons=(
-                      [
-                       Button.inline("Sudo", data="sudo")
-                      ],[
-                       Button.inline("Hami", data="hami")
-                      ],
-                    ),
-                    link_preview=False)
-
-@elnur.on(events.callbackquery.CallbackQuery(data="sudo"))
-async def handler(event):
-    await event.edit(f"📋",
-      buttons=(
-                      [
-                       Button.inline("Geri", data="helpdata")
-                      ],
-                    ),
-                    link_preview=False)
-
-
-@elnur.on(events.callbackquery.CallbackQuery(data="hami"))
-async def handler(event):
-    await event.edit(f"📋",
-      buttons=(
-                      [
-                       Button.inline("Geri", data="helpdata")
-                      ],
-                    ),
-                    link_preview=False)
-
 
 @elnur.on(events.NewMessage(pattern="^/help$"))
 @elnur.on(events.NewMessage(pattern="^/help@GenceliRoBot$"))
