9a2ef5788b08e6c810b2ada17aa327e782f14e23
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-04-18 23:09:57 +0400

    update start.py

--- Genceli/start.py
+++ Genceli/start.py
@@ -172,6 +172,7 @@ async def handler(event):
   sender = await event.get_sender()
 if sender.id == 5317589296:
      await client.send_message(event.chat_id,"Sen Sudo Deyilsən!")
+     return
     await event.edit(f"[{BOT_NAME}](https://t.me/{BOT_USERNAME}) Botun Əmirləri:\n\n/alive - Botun Sahibi Botu Aktiv Olduğuna Baxar.\n/stat - Botun Sahibi Botun Neçə Qrubda Olduğuna Baxar.",
       buttons=(
                       [
