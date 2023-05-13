d30e89b3a29964b36820fcec9c282bd2cface103
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2023-05-13 20:44:56 +0400

    update elnur.py

--- elnur.py
+++ elnur.py
@@ -1,15 +1,1 @@
-from telethon import TelegramClient 
-  
- # Config məlumatları 
-  
- # Telegram Client (Telethon) 
-API_ID = 29918051
-API_HASH = "793e62b1b2aefe53f84976d38215959e"
-bot_token = "5746131579:AAEL2ySw1sVRwsFdqekn9L4QO6mix6do9zE"
-SUDO_USERS = 5317589296
-OWNER_USERNAME = "ElnurGenCeLi"
-log_qrup = -1001875414285
-BOT_USERNAME = "GenceliRoBot"
-
- # Elnur
-elnur = TelegramClient('elnur', API_ID, API_HASH).start(bot_token=bot_token)
+reponu icazəsiz götürüb satan peysərdi !
