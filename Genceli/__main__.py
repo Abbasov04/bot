a84deec11767e98f12854dae22e4e729cf6722f1
ElikoAndMee <legendmuzkao99@gmail.com>
2023-06-05 08:40:02 +0400

    update __main__.py

--- Genceli/__main__.py
+++ Genceli/__main__.py
@@ -2,8 +2,8 @@ from telethon import TelegramClient, events
 from Genceli.start import *
 
 
-api_id = 20861666  # API ID-nizi burada daxil edin
-api_hash = '82539501ec5d09ac20d414e377cdefd9'  # API HASH-nizi burada daxil edin
+api_id = 14965050  # API ID-nizi burada daxil edin
+api_hash = '38bab2dab10fc1b6a9ba0bf683fd7048'  # API HASH-nizi burada daxil edin
 bot_token = '5597264378:AAFghbb1GYbSSKpqMeJ6a2LUMXYUb--M-Fs'  # Botunuzun tokeni burada daxil edin
 
 elnur = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
