b9cfa0e7af4209777855f35f17e33f2e7e53ff65
Abbasov04 <148398762+Abbasov04@users.noreply.github.com>
2024-03-30 01:41:18 +0400

    Update __main__.py

--- Genceli/__main__.py
+++ Genceli/__main__.py
@@ -4,7 +4,7 @@ from Genceli.start import *
 
 api_id = 14965050  # API ID-nizi burada daxil edin
 api_hash = '38bab2dab10fc1b6a9ba0bf683fd7048'  # API HASH-nizi burada daxil edin
-bot_token = ''  # Botunuzun tokeni burada daxil edin
+bot_token = '6565277854:AAHUD19Rf3VEBe7ZtDvUxxGEKPG0LswTy64'  # Botunuzun tokeni burada daxil edin
 
 elnur = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
 
